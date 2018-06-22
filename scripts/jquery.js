9e81eb8303eb249e9a14a2bf08347b68000b50da
Manon18 <maryland16z18@gmail.com>
2018-06-22 14:02:32 +0300

    added methods hide and show

---
+++ scripts/jquery.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+  $("#hide").click(function(){
+    $(".box-color-jquery").fadeOut()
+  });
+
+  $("#show").click(function(){
+    $(".box-color-jquery").fadeIn()
+  });
+});
